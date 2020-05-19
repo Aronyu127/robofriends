cac467f4e5d850bae596793f1c26bdba97719b1d
aronyu <h0935674938@hotmail.com>
2020-05-19 22:27:23 +0800

    Add scrollable for robots list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
+import Scroll from './Scroll';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => this.setState({ robots: users}));
+  }
+
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
   }
@@ -20,13 +27,20 @@ class App extends Component {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
 
-    return (
-      <div className='tc'>
-        <h1>RobotFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filterRobots}/>
-      </div>
-    );  
+    if(this.state.robots.length === 0){
+      return <h1 className='tc'>Loading...</h1>
+    }else {
+      return (
+        <div className='tc'>
+          <h1>RobotFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filterRobots}/>
+          </Scroll>
+        </div>
+      );  
+    }
+    
   }
   
 }
